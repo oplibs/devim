0b0b863fe14ff2d6ef7070b5c06d34ff023faf09
途海 <tuhai.whj@alibaba-inc.com>
2019-03-04 12:06:37 +0800

    MOD: Update for osx input plugin

--- init-osx.sh
+++ init-osx.sh
@@ -3,6 +3,12 @@
 echo "Initing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "Initing for xkbswitch plugin"
+git clone https://github.com/myshov/xkbswitch-macosx.git ~/.vim/xkbswitch-macosx
+cp ~/.vim/xkbswitch-macosx/bin/xkbswitch /usr/local/bin
+git clone https://github.com/myshov/libxkbswitch-macosx.git  ~/.vim/libxkbswitch-macosx
+cp ~/.vim/libxkbswitch-macosx/bin/libxkbswitch.dylib /usr/local/lib/
+
 echo "Initing fzf..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
