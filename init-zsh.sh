a132f527078e46de5d83c9cb567099c0a921b6c1
tuhai.whj <tuhai.whj@alibaba-inc.com>
2019-04-01 18:02:41 +0800

    ADD: Adding zsh shell

---
+++ init-zsh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#PROMPT='${ret_status} %{$fg[green]%}%n@$fg[red]%}%m:%{$fg[cyan]%}%d/>%{$reset_color%} $(git_prompt_info)'
+
+#install
+export SHELL="zsh"   # added to .zshrc
+git clone git://github.com/wting/autojump.git
+cd autojump
+./install.py #./uninstall.py
+cat '[[ -s /root/.autojump/etc/profile.d/autojump.sh ]] && source /root/.autojump/etc/profile.d/autojump.sh' >> ~/.zshrc
+cat 'autoload -U compinit && compinit -u' >> ~/.zshrc
