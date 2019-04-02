29cb1c602e45451b9cb42892babee0210a39f99b
tuhai.whj <tuhai.whj@alibaba-inc.com>
2019-04-02 12:04:41 +0800

    MOD: Update for zsh

--- init-zsh.sh
+++ init-zsh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#PROMPT='${ret_status} %{$fg[green]%}%n@$fg[red]%}%m:%{$fg[cyan]%}%d/>%{$reset_color%} $(git_prompt_info)'
+# PROMPT='${ret_status} %{$fg[green]%}%n@$fg[red]%}%m:%{$fg[cyan]%}%d/>%{$reset_color%} $(git_prompt_info)' >> .oh-my-zsh/themes/robbyrussell.zsh-theme
 
-#install
+#install autojump
 export SHELL="zsh"   # added to .zshrc
 git clone git://github.com/wting/autojump.git ~/
 cd ~/autojump
