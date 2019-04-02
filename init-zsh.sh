f7f0b846d5e2ae4f106727578519961cea813e9e
tuhai.whj <tuhai.whj@alibaba-inc.com>
2019-04-02 12:06:19 +0800

    MOD: Adding update to zsh

--- init-zsh.sh
+++ init-zsh.sh
@@ -10,3 +10,6 @@ cd ~/autojump
 ./install.py #./uninstall.py
 cat '[[ -s /root/.autojump/etc/profile.d/autojump.sh ]] && source /root/.autojump/etc/profile.d/autojump.sh' >> ~/.zshrc
 cat 'autoload -U compinit && compinit -u' >> ~/.zshrc
+
+cat '[[ -s /root/.autojump/etc/profile.d/autojump.sh ]] && source /root/.autojump/etc/profile.d/autojump.sh' >> ~/.bashrc
+cat 'autoload -U compinit && compinit -u' >> ~/.bashrc
