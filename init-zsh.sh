88e7637b45168519bd33ec95915c910b3e70b8e0
tuhai.whj <tuhai.whj@alibaba-inc.com>
2019-04-02 13:09:23 +0800

    MOD: Minor update

--- init-zsh.sh
+++ init-zsh.sh
@@ -5,11 +5,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 #install autojump
 export SHELL="zsh"   # added to .zshrc
-git clone git://github.com/wting/autojump.git ~/
+git clone git://github.com/wting/autojump.git ~/autojump
 cd ~/autojump
 ./install.py #./uninstall.py
-cat '[[ -s /root/.autojump/etc/profile.d/autojump.sh ]] && source /root/.autojump/etc/profile.d/autojump.sh' >> ~/.zshrc
-cat 'autoload -U compinit && compinit -u' >> ~/.zshrc
+# cat '[[ -s /root/.autojump/etc/profile.d/autojump.sh ]] && source /root/.autojump/etc/profile.d/autojump.sh' >> ~/.zshrc
+# cat 'autoload -U compinit && compinit -u' >> ~/.zshrc
 
 # cat '[[ -s /root/.autojump/etc/profile.d/autojump.sh ]] && source /root/.autojump/etc/profile.d/autojump.sh' >> ~/.bashrc
 # cat 'autoload -U compinit && compinit -u' >> ~/.bashrc
