0e38916925a1a33f0a4f32bf2220a01d784dba78
途海 <tuhai.whj@alibaba-inc.com>
2019-04-08 14:10:55 +0800

    MOD: Update for zshrc

--- init-zsh.sh
+++ init-zsh.sh
@@ -3,3 +3,8 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # PROMPT='${ret_status} %{$fg[green]%}%n@$fg[red]%}%m:%{$fg[cyan]%}%d/>%{$reset_color%} $(git_prompt_info)' >> .oh-my-zsh/themes/robbyrussell.zsh-theme
 
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+cp zshrc ~/.zshrc
+/bin/zsh
