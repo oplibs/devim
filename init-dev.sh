95452cc84b11f8d874b95ed7b0d94b3941b8d2c8
root <root@b31m01335.na61>
2019-04-09 07:35:29 +0800

    MOD: Update for some typo

--- init-dev.sh
+++ init-dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz $HOME/
-tar xvzf $HOME/go1.12.1.linux-amd64.tar.gz -C /usr/local
-rm -f $HOME/go1.12.1.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.12.1.linux-amd64.tar.gz ~/
+tar xvzf ~/go1.12.1.linux-amd64.tar.gz -C /usr/local
+rm -f ~/go1.12.1.linux-amd64.tar.gz
 echo "" >> ~/.zshrc
 echo "export GOROOT=/usr/local/go" >> ~/.zshrc
 echo "export GOPATH=~/projects/go" >> ~/.zshrc
--- init-zsh.sh
+++ init-zsh.sh
@@ -14,6 +14,6 @@ rm -rf ~/autojump
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-echo "plugins=(autojump web-search extract docker zsh_autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
+echo "plugins=(autojump web-search extract docker zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
 echo "[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
 echo "PROMPT='\${ret_status} %{\$fg[green]%}%n@\$fg[red]%}%m:%{\$fg[cyan]%}%d/>%{\$reset_color%} \$(git_prompt_info)'" >> ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
