fbeaad911bab25828b8905fba41c3a70051944c2
root <root@b31m01335.na61>
2019-04-08 23:03:30 +0800

    MOD: Fix the bug in install plugin for zsh

--- init-zsh.sh
+++ init-zsh.sh
@@ -8,6 +8,7 @@ export SHELL="zsh"   # added to .zshrc
 git clone git://github.com/wting/autojump.git ~/autojump
 cd ~/autojump
 ./install.py #./uninstall.py
+cd -
 rm -rf ~/autojump
 
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
@@ -15,4 +16,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 
 echo "plugins=(autojump web-search extract docker zsh_autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
 echo "[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
-echo "PROMPT='\${ret_status} %{\$fg[green]%}%n@\$fg[red]%}%m:%{\$fg[cyan]%}%d/>%{\$reset_color%} \$(git_prompt_info)'" >> .oh-my-zsh/themes/robbyrussell.zsh-theme
+echo "PROMPT='\${ret_status} %{\$fg[green]%}%n@\$fg[red]%}%m:%{\$fg[cyan]%}%d/>%{\$reset_color%} \$(git_prompt_info)'" >> ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
