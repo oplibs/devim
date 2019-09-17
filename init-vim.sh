e6af092d69ca4347373dda62109edf9b56d05ac6
tuhai.whj <tuhai.whj@alibaba-inc.com>
2019-09-17 09:03:09 +0800

    MOD: Adding vim script link

--- init-vim.sh
+++ init-vim.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
 
-# echo "Initing vim-plug..."
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-curl -sL install-node.now.sh/lts | sh
-# Optional install yarn if you want install extension by CocInstall command
-curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
-
-echo "Initing fzf..."
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-pip install pynvim
-# "wget https://jaist.dl.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz
-# "tar xzvf astyle_3.1_linux.tar.gz
-# "cd astyle_3.1_linux/build/gcc
-# "make
-# "sudo make install
+sh ./init-base.sh && sh ./init-ctags.sh && sh ./init-cquery.sh
