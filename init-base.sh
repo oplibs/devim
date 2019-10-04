95ac6c21206f68827ebcced093dbd4c51507796a
oplibs <oplibs@gmail.com>
2019-10-04 11:29:45 +0800

    MOD:Update pynvim

--- init-base.sh
+++ init-base.sh
@@ -12,6 +12,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 pip install pynvim
+pip3 install pynvim
 # "wget https://jaist.dl.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz
 # "tar xzvf astyle_3.1_linux.tar.gz
 # "cd astyle_3.1_linux/build/gcc
