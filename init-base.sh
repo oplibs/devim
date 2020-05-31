b4a1af6c382509fb690d670e5342cf485b12db9b
oplibs <oplibs@gmail.com>
2020-05-31 14:00:21 +0000

    MOD: More compatability

--- init-base.sh
+++ init-base.sh
@@ -3,15 +3,15 @@
 # echo "Initing vim-plug..."
 # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-curl -sL install-node.now.sh/lts | sh
+curl -sL install-node.now.sh/lts | /bin/bash
 # Optional install yarn if you want install extension by CocInstall command
-curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+curl --compressed -o- -L https://yarnpkg.com/install.sh | /bin/bash
 
 echo "Initing fzf..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-pip install pynvim
+#pip install pynvim
 pip3 install pynvim
 # "wget https://jaist.dl.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz
 # "tar xzvf astyle_3.1_linux.tar.gz
