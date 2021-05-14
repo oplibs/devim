418a1557f8137c7e05c97e5545150890016550a2
itrek <itrek@itreks-MBP.lan>
2021-05-15 07:49:40 +0800

    MOD: Adding plugin init

--- init-base.sh
+++ init-base.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# echo "Initing vim-plug..."
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Initing vim-plug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 curl -sL install-node.now.sh/lts -o lts.sh
 /bin/bash ./lts.sh --yes
