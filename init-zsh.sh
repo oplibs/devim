0e672fb8dfa00c78276ed5b776e2a692cc5f5d71
oplibs <oplibs@gmail.com>
2021-07-09 16:47:46 +0800

    MOD: Remove useless plugin

--- init-zsh.sh
+++ init-zsh.sh
@@ -15,7 +15,7 @@ git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zs
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
 
-echo "plugins=(autojump web-search extract docker zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)" >> ~/.zshrc
+echo "plugins=(autojump extract docker zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)" >> ~/.zshrc
 echo "export TERM=xterm-color" >> ~/.zshrc
 echo "[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
 echo "PROMPT='\${ret_status} %{\$fg[green]%}%n@\$fg[red]%}%m:%{\$fg[cyan]%}%d/>%{\$reset_color%} \$(git_prompt_info)'" >> ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
