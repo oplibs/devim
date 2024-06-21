e85538c54a9e516948274cd46325f937e8ce6a1a
Huajian Wang <huajianwang@microsoft.com>
2024-06-21 05:50:16 +0000

    MOD: Adding command checker

--- README.md
+++ README.md
@@ -16,27 +16,3 @@ The following software should be installed:
 * the_silver_searcher
 * ctags
 * cscope
-* cmake
-* astyle
-
-#### Javascript
-
-* Syntastic  javascript  npm install -g eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y eslint
-
-##### VIM Plugins
-
-* vim-jsbeautify
-
-##### Supporting utility plugin:
-
-#### react
-
-* vim-jsx
-
-    ```
-    let g:jsx_ext_required = 0 " Allow JSX in normal JS files
-    ```
-
-#### Plugins needed:
-
-#### HTML
--- init-base.sh
+++ init-base.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+command -v pip3 > /dev/null 2>&1 && {
+  echo "pip3 found !";
+} || {
+  command -v pip > /dev/null 2>&1 && {
+    echo "pip found !";
+  } || {
+    echo "Please install the pip, version 3 is prefered !";
+    exit 1;
+  }
+}
 echo "Initing vim-plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "Installing the nodejs and yarn ..."
 curl -sL install-node.now.sh/lts -o lts.sh
 /bin/bash ./lts.sh --yes
 rm -f lts.sh
@@ -17,6 +28,7 @@ echo "Initing fzf..."
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+echo "Installing the nodejs and yarn ..."
 if command -v pip >/dev/null 2>&1; then
     pip install pynvim
 else
@@ -27,9 +39,10 @@ if command -v pip3 >/dev/null 2>&1; then
 else
     echo 'no exists pip3'
 fi
+
 # "wget https://jaist.dl.sourceforge.net/project/astyle/astyle/astyle%203.1/astyle_3.1_linux.tar.gz
 # "tar xzvf astyle_3.1_linux.tar.gz
 # "cd astyle_3.1_linux/build/gcc
 # "make
 # "sudo make install
-cp ./.tmux.conf ~/
+# cp ./.tmux.conf ~/
