abb0aa314e76227e5c8cc1e0838562aa24d3017f
oplibs <oplibs@gmail.com>
2025-08-29 17:05:08 +0800

    MOD: New autojump on vim

--- init-zsh.sh
+++ init-zsh.sh
@@ -4,12 +4,12 @@
 # PROMPT='${ret_status} %{$fg[green]%}%n@$fg[red]%}%m:%{$fg[cyan]%}%d/>%{$reset_color%} $(git_prompt_info)' >> .oh-my-zsh/themes/robbyrussell.zsh-theme
 
 
-# export SHELL="zsh"   # added to .zshrc
-git clone https://github.com/wting/autojump.git ~/autojump
-cd ~/autojump
-./install.py #./uninstall.py
-cd -
-rm -rf ~/autojump
+# # export SHELL="zsh"   # added to .zshrc
+# git clone https://github.com/wting/autojump.git ~/autojump
+# cd ~/autojump
+# ./install.py #./uninstall.py
+# cd -
+# rm -rf ~/autojump
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
@@ -17,7 +17,8 @@ git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh
 
 echo "plugins=(autojump extract docker zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search)" >> ~/.zshrc
 echo "export TERM=screen" >> ~/.zshrc
-echo "[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
+# echo "[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh" >> ~/.zshrc
+echo "[[ -s /usr/share/autojump/autojump.sh ]] && source /usr/share/autojump/autojump.sh" >> ~/.zshrc
 echo "PROMPT='\${ret_status} %{\$fg[green]%}%n@\$fg[red]%}%m:%{\$fg[cyan]%}%d/>%{\$reset_color%} \$(git_prompt_info)'" >> ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
 
 echo "tmux source-file ~/.tmux.conf" >> ~/.zshrc
